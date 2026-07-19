c43917dfb293aa6c42b866c96e1475a1284986ff
mfderecho <248990622+mfderecho@users.noreply.github.com>
2026-07-19 10:56:14 +0900

    Add project info back to 76, remove image redirects (as image project moved to cloudflare)

--- build-prod.sh
+++ build-prod.sh
@@ -11,9 +11,16 @@ cp redirects .output/dist/_redirects || exit 1
 
 #cp -r 77/.output/public .output/dist/77
 
+# this is really horrible sedding the generated JS files but the map-app package is already generated with the hard url...
 bash -cx "cd 76 &&
 rm -rf .nuxt .output node-modules &&
 yarn install --immutable &&
+sed 's|https://www.komabasai.net||' 'node_modules/@komaba-festival-committee/map-app/dist/map-app.es.js' | 
+    sed 's|https://api.komabasai.net/76|/76/api|' > 'node_modules/@komaba-festival-committee/map-app/dist/map-app.es.js.tmp' &&
+mv 'node_modules/@komaba-festival-committee/map-app/dist/map-app.es.js.tmp' 'node_modules/@komaba-festival-committee/map-app/dist/map-app.es.js' &&
+sed 's|https://www.komabasai.net||' 'node_modules/@komaba-festival-committee/map-app/dist/map-app.umd.js' | 
+    sed 's|https://api.komabasai.net/76|/76/api|' > 'node_modules/@komaba-festival-committee/map-app/dist/map-app.umd.js.tmp' &&
+mv 'node_modules/@komaba-festival-committee/map-app/dist/map-app.umd.js.tmp' 'node_modules/@komaba-festival-committee/map-app/dist/map-app.umd.js' &&
 yarn generate:production;" || exit 1
 
 cp -r 76/.output/public .output/dist/76 || exit 1
@@ -23,6 +30,10 @@ echo "Copying KF76 geodata and map-style..."
 bash -cx "mkdir -p .output/dist/76/geodata &&
 cp 76mapapp/packages/map-geojson/geojson/*.geojson .output/dist/76/geodata &&
 cd 76mapapp/packages/map-style &&
+export VITE_GEODATA_BASE_URL=/76/geodata/ &&
+export VITE_MAPSTYLE_URL=/76/map-style/ &&
+export VITE_SPRITES_BASE_URL=/76/map-style/sprites/ &&
+export VITE_PROJECT_API_BASE_URL=/76/api/project/ &&
 pnpm install &&
 pnpm generate
 " || exit 1

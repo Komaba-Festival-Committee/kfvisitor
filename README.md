33db0503fb35b8cd0d3a104e8a026a66e28185ea
mfderecho <248990622+mfderecho@users.noreply.github.com>
2026-09-18 11:34:18 +0900

    [fix] Update error page seo, add instructions, copy to 77

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # kfvisitor
 駒場祭 visitor モノレポ
+
+# エラーページ生成のやり方
+
+ - 77に入る
+ - ブランチstatic-error-pageをチェックアウト
+ - pnpm generate
+ - `77/dist/77/static-error/`のファイルを404としてコピーする
--- build-qa.sh
+++ build-qa.sh
@@ -11,7 +11,8 @@ pnpm generate" || exit 1
 
 cp -r 77/dist/77 .output/dist/77 || exit 1
 
-cp error/* .output/dist
+cp -v error/* .output/dist
+cp -v error/* .output/dist/77
 
 # this is really horrible sedding the generated JS files but the map-app package is already generated with the hard url...
 bash -cx "cd 76 &&
